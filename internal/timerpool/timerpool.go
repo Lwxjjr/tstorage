cfeda474403247c88ebcf1da33ae2789de2db449
lwx <2213196412@qq.com>
2026-03-10 16:55:55 +0800

    docs: translate timerpool comments to Chinese

--- internal/timerpool/timerpool.go
+++ internal/timerpool/timerpool.go
@@ -7,28 +7,34 @@ import (
 
 var timerPool sync.Pool
 
-// Get returns a timer for the given duration d from the pool.
+// Get 从池中返回一个给定时长 d 的定时器。
 //
-// Return back the timer to the pool with Put.
+// 使用 Put 将定时器归还到池中。
 func Get(d time.Duration) *time.Timer {
 	if v := timerPool.Get(); v != nil {
 		t := v.(*time.Timer)
+		// 复用旧的 Timer，并设置新的时间
+		// 如果 Reset 返回 true，说明这个 Timer 还在跑
+		// 正常情况下 Put 进去的 Timer 都应该是停止的
 		if t.Reset(d) {
-			panic("active timer trapped to the pool!")
+			panic("活跃的定时器被捕获到池中！")
 		}
 		return t
 	}
+	// 池子里没东西，才创建一个新的
 	return time.NewTimer(d)
 }
 
-// Put returns t to the pool.
+// Put 将 t 归还到池中。
 //
-// t cannot be accessed after returning to the pool.
+// t 在归还到池中后不能再被访问。
 func Put(t *time.Timer) {
 	if !t.Stop() {
-		// Drain t.C if it wasn't obtained by the caller yet.
+		// 如果 t.Stop() 返回 false，说明定时器已经到期了，
+		// 它的通道 t.C 里可能已经塞进了一个时间值
+		//当定时器到时间（Expired）时，它会往自己的通道 t.C里塞进一个时间戳
 		select {
-		case <-t.C:
+		case <-t.C: // 排空通道
 		default:
 		}
 	}
